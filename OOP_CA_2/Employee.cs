15d3493b267a8b7e783bb86cad8fcf8caa4fbe64
Andrew Casey <60218234+AndrewC20@users.noreply.github.com>
2020-12-07 11:04:13 +0000

    Add project files.

---
+++ OOP_CA_2/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CA_2
+{
+    abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
